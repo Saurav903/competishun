09683f2296292ede16f83373e2736dc1a4899b9f
Saurav903 <sk8517167@gmail.com>
2023-07-14 16:51:21 +0530

    backend link added

--- task1/frontend/src/Pages/Home.jsx
+++ task1/frontend/src/Pages/Home.jsx
@@ -1,22 +1,17 @@
-import React from 'react'
-import { Box, Text } from '@chakra-ui/react'
-import { useNavigate } from 'react-router-dom';
+
+import { Box } from '@chakra-ui/react'
+import { Navigate } from 'react-router-dom';
 import Movie from './Movie';
 
 const Home = () => {
     let details = JSON.parse(localStorage.getItem("details"));
     console.log(details);
-    let navigate = useNavigate();
 
-    if(!details){
-        return navigate("/login");
-    }else {
-        return (
-            <Box>
-                <Movie/>
-            </Box>
-          )
-    }
+    return (
+        <Box>
+            {!details ? <Navigate to="/login" /> : <Movie/>}
+        </Box>
+      )
   
 }
 
